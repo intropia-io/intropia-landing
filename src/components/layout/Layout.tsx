430945b1c26e26597a0771388ca185ca8deb3754
Oleksii Sysoiev <bostonleek@gmail.com>
2023-01-26 20:57:40 +0200

    update styles

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Footer from "components/footer/footer";
+import Footer from "components/footer/Footer";
 import Header from "components/header/Header";
 
 type Props = {
