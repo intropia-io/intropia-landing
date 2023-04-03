b4d8ed3044f20a9e288f37b327c4083cc4924aef
Oleksii Sysoiev <bostonleek@gmail.com>
2023-04-03 10:30:36 +0300

    update return

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Layout from "./components/Layout";
 import Introduction from "./components/introduction/Introduction";
 
 function App() {
-  if(window.location.pathname === "/hire")
+  if(window.location.pathname === "/hire") {
     window.location.href = "https://cal.com/max-intropia/15min";
+    return 
+  }
   return (
     <Layout>
       <Introduction />
